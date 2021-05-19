394548fb812695278460e3325f2979950cba811a
timrebergen <tim.t.rebergen@student.hu.nl>
2021-05-19 10:31:36 +0200

    Initial commit

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from '@angular/core';
+import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import {Route} from '@angular/router';
+const routes: Routes = [
+  {
+    path: '',
+    loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
+  },
+  {
+    path: 'information',
+    loadChildren: () => import('./information/information.module').then( m => m.InformationPageModule)
+  },
+  {
+    path: 'stories',
+    loadChildren: () => import('./stories/stories.module').then( m => m.StoriesPageModule)
+  }
+];
+@NgModule({
+  imports: [
+    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
+  ],
+  exports: [RouterModule]
+})
+
+export class AppRoutingModule {}
